0c61630607b22667b351227a54f7f8e237b0139c
Jacob Folch <git.jacob.folch@gmail.com>
2020-03-03 22:39:50 +0100

    load saved bot

---
+++ botLoader.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+from whatsappConverter import SAVE_PATH
+from chatterbot import ChatBot
+
+class Bot:
+
+    def __init__(self, name):
+
+        if not os.path.isfile(os.path.join(SAVE_PATH, name + '.sqlite3')):
+            print('[!!] Couldnt find the bot...')
+            return
+        
+        self._name = name
+        self._bot = ChatBot(
+            name,
+            storage_adapter='chatterbot.storage.SQLStorageAdapter',
+            database_uri='sqlite:///' + os.path.join(SAVE_PATH, name + '.sqlite3')
+            )
+
+    def interact(self, s):
+        return self._name + ':' + str(self._bot.get_response(s))
+
+
+if __name__ == "__main__":
+    bot = Bot('test')
+
+    _in = input('>')
+
+    while _in.lower() != 'exit':
+        print(bot.interact(_in))
+        _in = input('>')
